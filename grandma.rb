e4718776a86eac840c99e46263f0bfcc965bb1ac
Ingrid sura <sura.ingrid@gmail.com>
2019-11-09 05:21:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,10 @@
-phrase="I LOVE YOU GRANDMA"
 def speak_to_grandma("shout,SHOUT,)
 if speak_to_grandma="shout"
   return "HUH?! SPEAK UP,SONNY!"
   elsif speak_to_grandma="SHOUT"
   return "NO,NOT SINCE 1938!"
 else return "I LOVE YOU TOO PUMPKIN!"
-
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
