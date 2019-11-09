51b9d5116c3afee74c41e9bbbc67884a25937e9d
Ingrid sura <sura.ingrid@gmail.com>
2019-11-09 05:14:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma("shout,SHOUT,")
-if speak_to_grandma("SHOUT")
+if speak_to_grandma"shout"
   return "HUH?! SPEAK UP,SONNY!"
-  elsif speak_to_grandma
+  elsif speak_to_grandma "SHOUT"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
